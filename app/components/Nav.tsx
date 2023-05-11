2c8220ed12d002a8d2d4e73be30a22102379e3f1
ジャイ子 <jak008@gmail.com>
2023-05-12 01:10:25 +0300

    - Pages can now have y overflow. - Removed <body> tags from layout to support layout pages. - Created app.js file to support layouts. - Moved page.tsx to pages/index.tsx . - Created News.tsx . - Added 2 demo graphics.

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -9,70 +9,70 @@ export default function Nav() {
   
   return (
     <nav className="">
-      <div className="flex justify-between items-center">
-      <Link href={process.env.navMainLink}>
-        <div className="flex flex-row gap-0 items-center">
-            <Image 
-              className="drop-shadow-xl"
-              src={"/img/logo.png"}
-              width={150}
-              height={150}
-              alt="Logo"
-            />
-            <h1 className="font-bold text-6xl -mx-[10vh] text-white drop-shadow-xl">{process.env.serverName}</h1>
-        </div>
-      </Link>
-      <div className="md:hidden flex items-center">
-
-        {/* Burger Menu Icon */}
-        <button onClick={toggleMenu}>
-          <svg xmlns="http://www.w3.org/2000/svg"fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-          <path strokeLinecap="round" className="text-white" strokeLinejoin="round" d="M3.75 5.25h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5" />
-          </svg>
-        </button>
+    <div className="flex justify-between items-center">
+    <Link href={process.env.navMainLink}>
+      <div className="flex flex-row gap-0 items-center">
+          <Image 
+            className="drop-shadow-xl"
+            src={"/img/logo.png"}
+            width={150}
+            height={150}
+            alt="Logo"
+          />
+          <h1 className="font-bold text-6xl -mx-[10vh] text-white drop-shadow-xl">{process.env.serverName}</h1>
       </div>
+    </Link>
+    <div className="md:hidden flex items-center">
 
-      {/* Desktop Menu  */}
-      <ul className="hidden md:flex items-center gap-6 text-white p-2 px-3 rounded-md bg-slate-300 bg-opacity-10 shadow-md ">
-        <Link href={process.env.navNewsLink} className="rounded-md hover:outline-dotted hover:text-yellow-200 outline-orange-400 py-1.5 px-2.5 drop-shadow-xl">
+      {/* Burger Menu Icon */}
+      <button onClick={toggleMenu}>
+        <svg xmlns="http://www.w3.org/2000/svg"fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+        <path strokeLinecap="round" className="text-white font-bold" strokeLinejoin="round" d="M3.75 5.25h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5" />
+        </svg>
+      </button>
+    </div>
+
+    {/* Desktop Menu  */}
+    <ul className="hidden md:flex items-center gap-6 text-white p-2 px-3 rounded-md bg-slate-300 bg-opacity-10 shadow-md ">
+      <Link href={process.env.navNewsLink} className="rounded-md hover:outline-dotted hover:text-yellow-200 outline-orange-400 py-1.5 px-2.5 drop-shadow-xl">
+          <li>News</li>
+      </Link>
+      <Link href={process.env.navDownloadLink} className="rounded-md hover:outline-dotted hover:text-yellow-200 outline-orange-400 py-1.5 px-2.5 drop-shadow-xl">
+          <li>Download</li>
+      </Link>
+      <Link href={process.env.navDiscordLink} className="rounded-md hover:outline-dotted hover:text-yellow-200 outline-orange-400 py-1.5 px-2.5 drop-shadow-xl">
+          <li>Discord</li>
+      </Link>
+      <Link href={process.env.navRegisterLink} className="rounded-md hover:outline-dotted hover:text-yellow-200 outline-orange-400 py-1.5 px-2.5 drop-shadow-xl">
+          <li className="font-bold">Register</li>
+      </Link>
+    </ul>
+    </div>
+
+
+    {/* Mobile Menu  */}
+    {isOpen ?
+      (
+        <div className="md:hidden">
+        <ul className="max-h-full max-w-12 gap-6 text-white p-2 px-3 rounded-md bg-slate-300 bg-opacity-10 shadow-md ">
+        <Link href={process.env.navNewsLink} className="block rounded-md hover:text-yellow-200 outline-orange-400 py-1.5 px-2.5 drop-shadow-xl">
             <li>News</li>
         </Link>
-        <Link href={process.env.navDownloadLink} className="rounded-md hover:outline-dotted hover:text-yellow-200 outline-orange-400 py-1.5 px-2.5 drop-shadow-xl">
+        <Link href={process.env.navDownloadLink} className="block rounded-md hover:text-yellow-200 outline-orange-400 py-1.5 px-2.5 drop-shadow-xl">
             <li>Download</li>
         </Link>
-        <Link href={process.env.navDiscordLink} className="rounded-md hover:outline-dotted hover:text-yellow-200 outline-orange-400 py-1.5 px-2.5 drop-shadow-xl">
+        <Link href={process.env.navDiscordLink} className="block rounded-md hover:text-yellow-200 outline-orange-400 py-1.5 px-2.5 drop-shadow-xl">
             <li>Discord</li>
         </Link>
-        <Link href={process.env.navRegisterLink} className="rounded-md hover:outline-dotted hover:text-yellow-200 outline-orange-400 py-1.5 px-2.5 drop-shadow-xl">
+        <Link href={process.env.navRegisterLink} className="block rounded-md hover:text-yellow-200 outline-orange-400 py-1.5 px-2.5 drop-shadow-xl">
             <li className="font-bold">Register</li>
         </Link>
-      </ul>
+        </ul>
       </div>
+      ) : ""
+    }
+    </nav>      
+  
 
-
-      {/* Mobile Menu  */}
-      {isOpen ?
-        (
-          <div className="md:hidden">
-          <ul className="max-h-full max-w-12 gap-6 text-white p-2 px-3 rounded-md bg-slate-300 bg-opacity-10 shadow-md ">
-          <Link href={process.env.navNewsLink} className="block rounded-md hover:text-yellow-200 outline-orange-400 py-1.5 px-2.5 drop-shadow-xl">
-              <li>News</li>
-          </Link>
-          <Link href={process.env.navDownloadLink} className="block rounded-md hover:text-yellow-200 outline-orange-400 py-1.5 px-2.5 drop-shadow-xl">
-              <li>Download</li>
-          </Link>
-          <Link href={process.env.navDiscordLink} className="block rounded-md hover:text-yellow-200 outline-orange-400 py-1.5 px-2.5 drop-shadow-xl">
-              <li>Discord</li>
-          </Link>
-          <Link href={process.env.navRegisterLink} className="block rounded-md hover:text-yellow-200 outline-orange-400 py-1.5 px-2.5 drop-shadow-xl">
-              <li className="font-bold">Register</li>
-          </Link>
-          </ul>
-        </div>
-        ) : ""
-      }
-          
-
-    </nav>
   )
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -9,19 +9,15 @@ export const metadata = {
   description: 'CosmicMS | 5x EXP, 2x Drop, 2x Meso | Join Us Today!',
 }
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function Layout({children}: {children: React.ReactNode}) {
   return (
-    <html lang="en">
-      <body className="mx-[2rem]">
-        <div className='p-2 m-4 max-w-full max-h-full xl:max-h-[42rem] h-screen bg-bg_image rounded-md'>
-          <Nav />
-          {children}
-        </div>
-      </body>
-    </html>
-  )
+    <>
+      <div className="mx-[2rem]">
+         <div className='p-2 m-4 max-w-full max-h-full xl:max-h-[42rem] h-screen bg-bg_image bg-scroll rounded-md'>
+           <Nav />
+           {children}
+         </div>
+      </div>
+    </>
+  );
 }
