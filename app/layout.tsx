2abddb473dfe68528f73d8206ff5071478928e18
JenyaIvanov <jak008@gmail.com>
2023-05-15 03:30:01 +0300

    - Cleaned globals.css - Cleaned and rewrote layout.tsx to support more web oriantations + desktop support. - Cleaned CSS on News.tsx .

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,5 @@
 import './globals.css'
-import { Inter } from 'next/font/google'
 import Nav from './components/Nav'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'CosmicMS',
@@ -11,13 +8,11 @@ export const metadata = {
 
 export default function Layout({children}: {children: React.ReactNode}) {
   return (
-    <>
-      <div className="mx-[2rem]">
-         <div className='p-2 m-4 max-w-full max-h-full xl:max-h-[42rem] h-screen bg-bg_image bg-scroll rounded-md'>
-           <Nav />
-           {children}
-         </div>
+    <main className='h-screen w-screen scale-[95%]'>
+      <div className=" rounded-md bg-bg_image backgroundImage bg-clone bg-cover h-screen w-screen p-5">
+            <Nav />
+            {children}
       </div>
-    </>
+    </main>
   );
 }
